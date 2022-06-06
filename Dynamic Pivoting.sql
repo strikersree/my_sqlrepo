b25faac918e0ccfc7e6d9c0989d5a6b14f164dd3
Srinivas Kannan <strikersree@users.noreply.github.com>
2022-06-06 10:38:57 +0530

    Added Dynamic Pivoting Store Procedure
    The following is a store procedure that takes input such as table name, pivot column, aggregation and non-aggregated column and outputs as a pivot table. The code is intended to work only on MySQL and MariaDB since there is no inbuilt PIVOT function, but can be tweaked to accommodate any SQL flavors.

---
+++ Dynamic Pivoting.sql
@@ -0,0 +1,79 @@
+
+
+
+
+CREATE OR REPLACE PROCEDURE `dynamic_pivot`(IN mastercolumn VARCHAR(70), IN pivotcolumn VARCHAR(100), IN tablename VARCHAR(50), IN agg_func CHAR(10), IN agg_col VARCHAR(20) )
+
+BEGIN 
+	-- convert pivot column into list
+	EXECUTE IMMEDIATE CONCAT('select GROUP_CONCAT(distinct ',pivotcolumn, ' SEPARATOR ";") into @cols FROM ',tablename);
+
+	EXECUTE IMMEDIATE CONCAT('select COUNT(distinct `',pivotcolumn,'`) INTO @maxx FROM ',tablename,' ;');
+
+	SET @minn = - 1 ;
+	SET @factor = -1;
+	SET @maxx = @maxx * @factor ;
+
+	EXECUTE IMMEDIATE CONCAT('CREATE TEMPORARY TABLE temp ( `',mastercolumn,'` VARCHAR(100) NULL )',';');
+	
+	-- Add pivot columns dynamically 
+	WHILE  @minn >= @maxx DO  
+	
+		EXECUTE IMMEDIATE CONCAT('ALTER TABLE temp ADD COLUMN ', '`', SUBSTRING_INDEX(SUBSTRING_INDEX(@cols, ';', @minn), ';', 1),'`', ' VARCHAR(200) NULL DEFAULT ''0'' ;');
+		SET @minn = @minn - 1 ;	
+	
+	END WHILE; 
+
+	SET @minn = - 1 ;
+
+--  store pivot data into temp
+	WHILE  @minn >= @maxx DO 
+	
+		IF agg_func = 'COUNT' 
+		THEN
+			EXECUTE IMMEDIATE CONCAT('INSERT INTO temp( `',mastercolumn,'`,','`',SUBSTRING_INDEX(SUBSTRING_INDEX(@cols, ';', @minn), ';', 1),'`',')',' SELECT distinct `',mastercolumn,'` ,COUNT(CASE WHEN `',pivotcolumn,'` = "',SUBSTRING_INDEX(SUBSTRING_INDEX(@cols, ';', @minn), ';', 1),'"',' THEN 1 ELSE NULL END) AS ', '`',SUBSTRING_INDEX(SUBSTRING_INDEX(@cols, ';', @minn), ';', 1), '`',' FROM ',tablename,' GROUP BY `',mastercolumn,'`;');
+			SET @minn = @minn - 1 ;
+		ELSE 
+			EXECUTE IMMEDIATE CONCAT('INSERT INTO temp( `',mastercolumn,'`,','`',SUBSTRING_INDEX(SUBSTRING_INDEX(@cols, ';', @minn), ';', 1),'`',')',' SELECT distinct `',mastercolumn,'` ,CASE WHEN `',pivotcolumn,'` = "',SUBSTRING_INDEX(SUBSTRING_INDEX(@cols, ';', @minn), ';', 1),'"',' THEN ',agg_func,'(',agg_col,') ELSE NULL END AS ', '`',SUBSTRING_INDEX(SUBSTRING_INDEX(@cols, ';', @minn), ';', 1), '`',' FROM ',tablename,' GROUP BY `',mastercolumn,'`;');
+			SET @minn = @minn - 1 ;
+		END IF ;
+	
+	END WHILE;
+
+	CREATE TEMPORARY TABLE pivot_results AS (SELECT * FROM temp WHERE 1=2);
+
+	SET @minn = - 1 ;
+
+	SET @groupcolumn = '';
+	
+	SET @groupquery = CONCAT('SELECT `',mastercolumn,'` ,');
+
+	WHILE  @minn >= @maxx DO 
+		IF @minn = @maxx THEN 
+			SET @groupcolumn = CONCAT('MAX(`', SUBSTRING_INDEX(SUBSTRING_INDEX(@cols, ';', @minn), ';', 1), '`) AS `',SUBSTRING_INDEX(SUBSTRING_INDEX(@cols, ';', @minn), ';', 1),'` FROM temp GROUP BY `',mastercolumn, '`;') ;
+		ELSE 
+			SET @groupcolumn = CONCAT('MAX(`', SUBSTRING_INDEX(SUBSTRING_INDEX(@cols, ';', @minn), ';', 1), '`) AS `',SUBSTRING_INDEX(SUBSTRING_INDEX(@cols, ';', @minn), ';', 1),'`,') ;
+		END IF ;
+		SET @groupquery = CONCAT(@groupquery,@groupcolumn) ;
+		SET @minn = @minn - 1 ;
+	END WHILE; 
+
+	EXECUTE IMMEDIATE CONCAT('INSERT INTO pivot_results ', @groupquery); 
+
+	EXECUTE IMMEDIATE 'DROP TABLE temp';
+
+	EXECUTE IMMEDIATE 'SELECT * FROM pivot_results ;';
+
+
+END
+
+
+
+-- Test 
+
+CALL dynamic_pivot('account_rep','booking_sales_mtn','TSG_Cross_Sell', 'SUM','local_amount');
+
+
+
+
+
